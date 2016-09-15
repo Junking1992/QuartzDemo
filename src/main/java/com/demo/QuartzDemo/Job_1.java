49521690d68029bafaecb066624522c23395a102
Samsung_junking <junking.wangjun@gmail.com>
2016-09-15 16:14:05 +0800

    Signed-off-by: Samsung_junking <junking.wangjun@gmail.com>

---
+++ src/main/java/com/demo/QuartzDemo/Job_1.java
@@ -0,0 +1,19 @@
+package com.demo.QuartzDemo;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Job_1 implements Job {
+
+	private static Logger _log = LoggerFactory.getLogger(Job_1.class);
+
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		_log.info("Hello World! - " + new Date());
+	}
+
+}
--- src/test/java/com/demo/QuartzDemo/AppTest.java
+++ src/test/java/com/demo/QuartzDemo/AppTest.java
@@ -1,38 +1,15 @@
 package com.demo.QuartzDemo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void demo_1() throws Exception{
+		Demo_1 demo = new Demo_1();
+		demo.run();
+	}
 }
